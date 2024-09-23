d5933d0bef57e904a3e3cee6082f18d7d7000b57
Reno Satya Adrian <renosatyaadrian@gmail.com>
2024-09-23 14:58:19 +0700

    Add Initial Web

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Bloomkergs.Pages;
+// namespace Bloomkergs.Pages;
 
 public class IndexModel : PageModel
 {
--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Bloomkergs.Pages;
+// namespace Bloomkergs.Pages;
 
 public class PrivacyModel : PageModel
 {
