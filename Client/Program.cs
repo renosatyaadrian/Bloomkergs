3abe40d297f7f43c03e81f125d5c8a91c08fc8f6
Reno Satya Adrian <renosatyaadrian@gmail.com>
2024-10-05 20:04:35 +0700

    Refactor Foldering

---
+++ Client/Program.cs
@@ -0,0 +1,31 @@
+// using Client.;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddRazorPages(); 
+builder.Services.AddControllers();
+builder.Services.AddHttpClient(); 
+
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles(); 
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllers(); 
+app.MapRazorPages();  
+
+app.Run();
