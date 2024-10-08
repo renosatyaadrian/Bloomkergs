65959763fd41481f1fccc0d8368517d62751a7cb
Reno Satya Adrian <renosatyaadrian@gmail.com>
2024-10-08 16:16:30 +0700

    Remove unused

--- Client/Views/Index.cshtml.cs
+++ Client/Views/Index.cshtml.cs
@@ -1,14 +1,8 @@
-using Client.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Newtonsoft.Json;
-
-// namespace Bloomkergs.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly HttpClient _httpClient;
-
-    // public List<Article> Articles { get; set; }
 
     public IndexModel(HttpClient httpClient)
     {
@@ -17,16 +11,5 @@ public class IndexModel : PageModel
 
     public async Task OnGetAsync()
     {
-        // var response = await _httpClient.GetAsync("http://localhost:5000/api/Articles");
-        
-        // if (response.IsSuccessStatusCode)
-        // {
-        //     var articlesJson = await response.Content.ReadAsStringAsync();
-        //     Articles = JsonConvert.DeserializeObject<List<Article>>(articlesJson);
-        // }
-        // else
-        // {
-        //     Articles = new List<Article>(); 
-        // }
     }
 }
